0f7d1e0d12c1620f937a1ab37feea555b84cf64b
James Petty <jpetty@harrys.com>
2016-01-06 10:51:15 -0500

    Since changes to the underlying executor behavior have been made, this is now a new patch version

--- hyppo-test/build.sbt
+++ hyppo-test/build.sbt
@@ -5,4 +5,7 @@ name := "hyppo-scala-test"
 //  Bring in shared SBT configurations from project root
 Common.buildSettings
 
+//  Since this is not "ready" yet, all versions are snapshots
+version := Common.snapshotVersion
+
 libraryDependencies += "com.harrys.hyppo" % "source-api" % Common.hyppoVersion
--- project/Common.scala
+++ project/Common.scala
@@ -3,7 +3,19 @@ import Keys._
 
 
 object Common {
-  final val hyppoVersion  = "0.6.3"
+  final val hyppoVersion  = "0.6.4"
+
+  /**
+    * @return A "-SNAPSHOT" suffixed version of the current version, even if the current version is not
+    *         a snapshot.
+    */
+  def snapshotVersion = {
+    if (hyppoVersion.endsWith("-SNAPSHOT")){
+      hyppoVersion
+    } else {
+      hyppoVersion + "-SNAPSHOT"
+    }
+  }
 
   final val buildSettings = Seq(
     crossPaths        := false,
